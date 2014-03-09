a440647b5f037160b057e178d10dc1faa2a8cf55
Joseph Nix <nixjdm@terminallabs.com>
2014-03-08 22:52:08 -0600

    Added check for a tie scenario. Added check for a winning or tie scenario at the end of a turn. Added method for computer's turn (which is only manual right now). Made successive turns be prompted for if the game isn't over

--- tictactoe.py
+++ tictactoe.py
@@ -1,4 +1,4 @@
-from numpy import array, flipud, identity, trace, zeros
+from numpy import array, count_nonzero, flipud, identity, trace, zeros
 
 #TODO: remove, this is a standin until there's sufficient interaction.
 
@@ -6,52 +6,90 @@ from numpy import array, flipud, identity, trace, zeros
 class TicTacToe(object):
     def __init__(self):
         self.game_state = zeros((3,3))
-        # self.game_state = array( [[-1,0,1],[1,-1,0],[1,0, -1]] )
+        # self.game_state = array( [[0,-1,1],[-1,-1,1],[1,1,-1]] )
 
     def check_for_win(self):
         '''
         Assume a valid board configuration, i.e. only 0 or 1 winners
         '''
 
-        I = identity(3) # define 3x3 identity matrix
-        ones = array( [[1,1,1]] ) # define 1s vector (1x3 matrix)
+        I = identity(3) # Define 3x3 identity matrix
+        ones = array( [[1,1,1]] ) # Define 1s vector (1x3 matrix)
         column = row = 0 # numpy indices start at 0, not 1
-        #check the columns
+        # Check the columns
         while column < 3:
             column_sum = ones.dot(self.game_state).dot(I[:,column])
             if column_sum == 3:
-                return 'x wins1'
+                return 'win'
             elif column_sum == -3:
-                return 'o wins1'
+                return 'win'
             column += 1
         # Check the rows
         while row < 3:
             row_sum = ones.dot(self.game_state.T).dot(I[:,row])
             if row_sum == 3:
-                return 'x wins'
+                return 'win'
             elif row_sum == -3:
-                return 'o wins'
+                return 'win'
             row += 1
         # Check the diaganols
         if trace(self.game_state) == 3:
-            return 'x wins'
+            return 'win'
         elif trace(self.game_state) == -3:
-            return 'o wins'
+            return 'win'
         elif trace(flipud(self.game_state)) == 3:
-            return 'x wins'
+            return 'win'
         elif trace(flipud(self.game_state)) == -3:
-            return 'o wins'
-        else: return 'no winner, keep playing'
+            return 'win'
+        # Check for tie game
+        elif count_nonzero(self.game_state) == 9:
+            return 'tie'
+        # Keep playing
+        else: return 'no winner'
 
     def human_turn(self):
         # Assume humans are X (and X equals 1)
-        row = raw_input("Your turn! Enter a row.")
-        column = raw_input("Your turn! Enter a column.")
-
+        row = raw_input("your turn! Enter a row.")
+        column = raw_input("your turn! Enter a column.")
+        # place move
         if self.check_position(row, column):
             self.game_state[row,column] = 1 # TODO generalize to Xs or Os
+            print self.game_state
         else:
             print "That spots already taken! Please pick another spot."
+            self.human_turn()
+        # check for win
+        win_state = self.check_for_win()
+        if win_state == 'win':
+            print 'game over - someone won!'
+        elif win_state == 'tie':
+            print "game over - there's a tie"
+        elif win_state == 'no winner':
+            print 'should now keep playing'
+            # computer's turn
+            self.computer_turn()
+
+    def computer_turn(self):
+        ##TODO: This is actually manual (human) right now, will add AI after game flow is finished
+        # Assume humans are O (and O equals -1).
+        row = raw_input("computer's turn! Enter a row.")
+        column = raw_input("computer's turn! Enter a column.")
+        # place move
+        if self.check_position(row, column):
+            self.game_state[row,column] = -1 # TODO generalize to Xs or Os
+            print self.game_state
+        else:
+            print "That spots already taken! Please pick another spot."
+            self.computer_turn()
+        # check for win
+        win_state = self.check_for_win()
+        if win_state == 'win':
+            print 'game over - someone won!'
+        elif win_state == 'tie':
+            print "game over - there's a tie"
+        elif win_state == 'no winner':
+            print 'should now keep playing'
+            # computer's turn
             self.human_turn()
 
     def check_position(self, row, column):
@@ -64,5 +102,3 @@ game = TicTacToe()
 print game.game_state
 game.human_turn()
 print game.game_state
-result = game.check_for_win()
-print result

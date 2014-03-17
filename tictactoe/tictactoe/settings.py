a0a937b6ed61649fb8f058958a9c2b44d56b5338
Joseph Nix <nixjdm@terminallabs.com>
2014-03-16 21:27:53 -0500

    Modifying settings.py for static files and assets dirs, added images for x, o, and board.

--- tictactoe/tictactoe/settings.py
+++ tictactoe/tictactoe/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-print BASE_DIR
+STATIC_ROOT = os.path.join(BASE_DIR, 'tictactoe/static')
+ASSETS = os.path.join(BASE_DIR, 'tictactoe/assets')
+
+STATICFILES_DIRS = (ASSETS, )
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
